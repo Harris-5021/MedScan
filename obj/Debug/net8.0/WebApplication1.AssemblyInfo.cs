ff6ab5317d47185e913534732b3e23057944c732
Harris-5021 <harrisfiaz3@gmail.com>
2024-09-03 20:08:18 +0100

    commit

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7383c8fb5674d4c0f73c8d524d64aaf8482fc843")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+716e3da3f66e8a5da53923629d88cdaa51b1a55c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
