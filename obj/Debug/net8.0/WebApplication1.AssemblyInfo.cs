e2cec26cd78b9ebf573a2a4e701a5bc02ba63fbf
Harris-5021 <harrisfiaz3@gmail.com>
2024-09-04 16:02:43 +0100

    dev to production

--- obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa04b1f75472d81f83da2fac821ae5babea5a37a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9bdfbe31fa30aaea5951c7660c3e85af29608a24")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
